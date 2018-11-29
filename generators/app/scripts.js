f461d582e38f0ec0edc5e396232697ecfc74995d
Paulo McNally <paulomcnally@gmail.com>
2018-11-29 15:57:55 -0600

    Fix code style

--- generators/app/scripts.js
+++ generators/app/scripts.js
@@ -2,8 +2,8 @@ module.exports = {
   start: 'webpack-dev-server',
   eslint: 'node_modules/eslint/bin/eslint.js . --ext .js',
   docs: 'NODE_ENV=production webpack -p',
-  build: `babel src -d dist --ignore '**/*.test.js'`,
+  build: 'babel src -d dist --ignore \'**/*.test.js\'',
   clean: 'rm -rf dist',
   dist: 'npm run clean && npm run build',
-  test: 'yarn run dist && jest'
+  test: 'yarn run dist && jest',
 };
